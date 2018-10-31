41b81b0f17cb19747645422d10aa4efdf219c911
TeddybearCrisis <soenkegreve@gmail.com>
2018-10-31 15:43:11 +0100

    removed quiet flags from cmake script to see errors on jenkins

--- cmake/tools.cmake
+++ cmake/tools.cmake
@@ -38,7 +38,8 @@ macro(initGitSubmodule PATH VERSION)
                 execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --depth 1 --init
                                 WORKING_DIRECTORY ${PATH}
                                 RESULT_VARIABLE GIT_SUBMOD_RESULT
-                                OUTPUT_QUIET)
+                                )
+                    #OUTPUT_QUIET
                 if (NOT GIT_SUBMOD_RESULT EQUAL "0")
                     message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT},
                 please checkout submodules")
@@ -46,10 +47,11 @@ macro(initGitSubmodule PATH VERSION)
 
                 # fetch the specific version
                 message(STATUS "${PATH} checking out version: ${VERSION}")
-                execute_process(COMMAND ${GIT_EXECUTABLE} checkout -q ${VERSION}
+                execute_process(COMMAND ${GIT_EXECUTABLE} checkout  ${VERSION}
                                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/${PATH}
                                 RESULT_VARIABLE GIT_SUBMOD_CHECKOUT_RESULT
-                                OUTPUT_QUIET)
+                                )
+                    #-q  OUTPUT_QUIET
 
                 if (NOT GIT_SUBMOD_CHECKOUT_RESULT EQUAL "0")
                     message(FATAL_ERROR "git checkout ${VERSION} in ${PATH} failed with ${GIT_SUBMOD_CHECKOUT_RESULT},
